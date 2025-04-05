ee95ec72700c623714eb0ff0a257199ef63bde8c
ngoc thacch <huynhngocthach2004@gmail.com>
2025-04-05 16:33:05 +0700

    initial project Web Chat

---
+++ webChatBackend/src/main/java/com/backend/webChatBackend/WebChatBackendApplication.java
@@ -0,0 +1,13 @@
+package com.backend.webChatBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebChatBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebChatBackendApplication.class, args);
+	}
+
+}
---
+++ webChatBackend/src/test/java/com/backend/webChatBackend/WebChatBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.webChatBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebChatBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
